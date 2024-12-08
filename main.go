3ef3991c6a48815bf86dc2d7a0d108bf4ec7aa41
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-12-08 23:21:41 +0800

    additions: setup postman and try first API without DB (get request)

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ import (
 
 func main() {
 	fmt.Println("Hello, World. Cat win")
-
 	app := fiber.New()
+
+	// first route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status((200)).JSON(fiber.Map{"msg": "hello world!"})
+	})
 
 	log.Fatal(app.Listen(":4000"))
 }
